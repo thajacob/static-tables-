8a2fc367f4826da743a62795521f9a4150204605
thajacob <tha.jacob2@gmail.com>
2017-02-23 11:56:40 +0000

    First commit

---
+++ Static Tables/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Static Tables
+//
+//  Created by jakub skrzypczynski on 30/09/2016.
+//  Copyright © 2016 test project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Static Tables/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Static Tables
+//
+//  Created by jakub skrzypczynski on 30/09/2016.
+//  Copyright © 2016 test project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
